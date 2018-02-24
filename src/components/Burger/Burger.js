6579e740029e8dabb71f1459c8405a72e1f97501
marcpre <marcus.presich@gmail.com>
2018-02-24 17:10:12 +0100

    create BurgerIntegredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,40 +1,17 @@
 import React from 'react'
 
-import classes from './BurgerIngredients.css'
-import PropTypes from 'prop-types'
+import classes from './Burger.css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burgerIngredient = (props) => {
-    let ingredient = null;
-    
-    switch(props.type) {
-        case ('bread-bottom'): 
-            ingredient = <div className={classes.BreadBottom}></div>
-            break;
-        case ('bread-top'):
-            ingredient = (
-                <div className={classes.BreadTop}>
-                    <div className={classes.Seeds1}></div>
-                    <div className={classes.Seeds2}></div>
-                </div>
-                );
-            break;
-        case ('meat'):
-            ingredient = <div className={classes.Meat}></div>
-            break;    
-        case ('cheese'):
-            ingredient = <div className={classes.Cheese}></div>
-            break;        
-        case ('bacon'):
-            ingredient = <div className={classes.Bacon}></div>
-            break;        
-        case ('salad'):
-            ingredient = <div className={classes.Salad}></div>
-            break;        
-        default: 
-            ingredient = null;
-    }
-    return ingredient;
-    
+const Burger = (props) => {
+    return ( 
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
 }
 
-export default burgerIngredient;
+export default Burger;
