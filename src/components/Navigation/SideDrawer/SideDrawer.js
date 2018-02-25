b8e74378a2a19cfe61c33d135496df353b3829b6
marcpre <marcus.presich@gmail.com>
2018-02-25 13:21:52 +0100

    fixing logo

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../../Logo/Logo';
+import classes from './Sidedrawer.css'
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const sideDrawer = (props) => {
+    return(
+        <div className={classes.SideDrawer}>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <nav>
+                <NavigationItems />
+            </nav>                
+        </div>    
+    );
+}
+
+export default sideDrawer
