b3faabec5a881e3f60fe01fba8b317b7f2ad29d1
marcpre <marcus.presich@gmail.com>
2018-03-04 09:43:58 +0100

    adding orders component + linking

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 
-class Order extends Component {
+import Order from '../../components/Order/Order'
 
+class Orders extends Component {
+    render() {
+        return (
+            <div>
+                <Order />
+                <Order />                
+            </div>  
+        )
+    }
 }    
     
-export default Order;
+export default Orders;
