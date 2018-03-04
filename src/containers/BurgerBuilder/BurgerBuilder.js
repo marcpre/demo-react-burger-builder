728932041c72062c1cf1b83d781007cf1f1f56cd
marcpre <marcus.presich@gmail.com>
2018-03-04 10:10:11 +0100

    fixing orders + ordersummary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,28 +30,29 @@ class BurgerBuilder extends Component {
         error: false
     }
 
-    componentDidMount() {
-        axios.get('https://fir-react-burger-builder.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ ingredients: response.data });
-            })
-            .catch(error => {
-                this.setState({ error: true });
-            });
+    componentDidMount () {
+        console.log(this.props);
+        axios.get( 'https://fir-react-burger-builder.firebaseio.com/ingredients.json' )
+            .then( response => {
+                this.setState( { ingredients: response.data } );
+            } )
+            .catch( error => {
+                this.setState( { error: true } );
+            } );
     }
 
-    updatePurchaseState(ingredients) {
-        const sum = Object.keys(ingredients)
-            .map(igKey => {
+    updatePurchaseState ( ingredients ) {
+        const sum = Object.keys( ingredients )
+            .map( igKey => {
                 return ingredients[igKey];
-            })
-            .reduce((sum, el) => {
+            } )
+            .reduce( ( sum, el ) => {
                 return sum + el;
-            }, 0);
-        this.setState({ purchasable: sum > 0 });
+            }, 0 );
+        this.setState( { purchasable: sum > 0 } );
     }
 
-    addIngredientHandler = (type) => {
+    addIngredientHandler = ( type ) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
         const updatedIngredients = {
@@ -61,13 +62,13 @@ class BurgerBuilder extends Component {
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
-        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-        this.updatePurchaseState(updatedIngredients);
+        this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
+        this.updatePurchaseState( updatedIngredients );
     }
 
-    removeIngredientHandler = (type) => {
+    removeIngredientHandler = ( type ) => {
         const oldCount = this.state.ingredients[type];
-        if (oldCount <= 0) {
+        if ( oldCount <= 0 ) {
             return;
         }
         const updatedCount = oldCount - 1;
@@ -78,43 +79,44 @@ class BurgerBuilder extends Component {
         const priceDeduction = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
-        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-        this.updatePurchaseState(updatedIngredients);
+        this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
+        this.updatePurchaseState( updatedIngredients );
     }
 
     purchaseHandler = () => {
-        this.setState({ purchasing: true });
+        this.setState( { purchasing: true } );
     }
 
     purchaseCancelHandler = () => {
-        this.setState({ purchasing: false });
+        this.setState( { purchasing: false } );
     }
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-           const queryParams = []
-           for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients))
-            }
-            queryParams.push('price=' + this.state.totalPrice)
-            const queryString = queryParams.join('&')
-           this.props.history.push({
-                pathname: '/checkout',
-                search: '?' + queryString
-            })
+        
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
-    render() {
+    render () {
         const disabledInfo = {
             ...this.state.ingredients
         };
-        for (let key in disabledInfo) {
+        for ( let key in disabledInfo ) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
         let orderSummary = null;
         let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
 
-        if (this.state.ingredients) {
+        if ( this.state.ingredients ) {
             burger = (
                 <Aux>
                     <Burger ingredients={this.state.ingredients} />
@@ -133,7 +135,7 @@ class BurgerBuilder extends Component {
                 purchaseCancelled={this.purchaseCancelHandler}
                 purchaseContinued={this.purchaseContinueHandler} />;
         }
-        if (this.state.loading) {
+        if ( this.state.loading ) {
             orderSummary = <Spinner />;
         }
         // {salad: true, meat: false, ...}
@@ -148,4 +150,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default withErrorHandler(BurgerBuilder, axios);
+export default withErrorHandler( BurgerBuilder, axios );
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import Button from '../../../components/UI/Button/Button'
-import classes from './ContactData.css'
-import Spinner from '../../../components/UI/Spinner/Spinner'
-import axios from '../../../axios-orders'
+import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
     state = {
@@ -15,56 +15,54 @@ class ContactData extends Component {
         },
         loading: false
     }
-    
-    orderHandler = (event) => {
-        event.preventDefaul();
-        this.setState({ loading: true });
+
+    orderHandler = ( event ) => {
+        event.preventDefault();
+        this.setState( { loading: true } );
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
             customer: {
-                name: 'Test Tester',
+                name: 'Max Schwarzmüller',
                 address: {
                     street: 'Teststreet 1',
-                    zipCode: '7893',
-                    country: 'Austria'
+                    zipCode: '41351',
+                    country: 'Germany'
                 },
                 email: 'test@test.com'
             },
             deliveryMethod: 'fastest'
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-                this.props.history.push('/')
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState( { loading: false } );
+                this.props.history.push('/');
+            } )
+            .catch( error => {
+                this.setState( { loading: false } );
+            } );
     }
-    
-    render() {
+
+    render () {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-                <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-                <input className={classes.Input} type="street" name="street" placeholder="Your street" />
-                <input className={classes.Input} type="postal" name="postal" placeholder="Your postal" />
-                <Button 
-                btnType="Success"
-                clicked={this.orderHandler}> ORDER </Button>    
-            </form>   
-        )
-        if (this.state.loading) {
-            form = <Spinner />
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if ( this.state.loading ) {
+            form = <Spinner />;
         }
-        return(
+        return (
             <div className={classes.ContactData}>
-                <h4>Enter your Contact Data: </h4>
+                <h4>Enter your Contact Data</h4>
                 {form}
             </div>
-        )
+        );
     }
 }
 
-export default ContactData
+export default ContactData;
