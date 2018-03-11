beffa12efd68963090fe52efa2474aa7a9eb3872
marcpre <marcus.presich@gmail.com>
2018-03-11 13:11:55 +0100

    adding auth check + if user exists

--- src/containers/auth/Auth.js
+++ src/containers/auth/Auth.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
-import Spinner from '../../components/UI/Spinner/Spinner'
-
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 
@@ -41,6 +41,12 @@ class Auth extends Component {
             }
         },
         isSignup: true
+    }
+
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
     }
 
     checkValidity ( value, rules ) {
@@ -118,20 +124,27 @@ class Auth extends Component {
                 touched={formElement.config.touched}
                 changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
         ) );
-        
-        if(this.props.loading) {
-            form = <Spinner/>
+
+        if (this.props.loading) {
+            form = <Spinner />
         }
 
-        let errorMessage = null
-        if(this.props.error) {
+        let errorMessage = null;
+
+        if (this.props.error) {
             errorMessage = (
                 <p>{this.props.error.message}</p>
-            )
+            );
         }
-        
+
+        let authRedirect = null;
+        if (this.props.isAuthenticated) {
+            authRedirect = <Redirect to={this.props.authRedirectPath}/>
+        }
+
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -145,17 +158,21 @@ class Auth extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) )
-    };
-};
-
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
-    }
-}
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) ),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
+    };
+};
 
 export default connect( mapStateToProps, mapDispatchToProps )( Auth );
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 import Aux from '../util';
 import classes from './Layout.css';
@@ -25,10 +25,10 @@ class Layout extends Component {
         return (
             <Aux>
                 <Toolbar
-                    isAuth={this.props.isAuthenticated} 
+                    isAuth={this.props.isAuthenticated}
                     drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer
-                    isAuth={this.props.isAuthenticated} 
+                    isAuth={this.props.isAuthenticated}
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
@@ -42,7 +42,7 @@ class Layout extends Component {
 const mapStateToProps = state => {
     return {
         isAuthenticated: state.auth.token !== null
-    }
-}
+    };
+};
 
-export default connect(mapStateToProps)(Layout);
+export default connect( mapStateToProps )( Layout );
